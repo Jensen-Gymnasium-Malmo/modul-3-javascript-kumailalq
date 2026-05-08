0d3ee2540a33ee327328c1063a6271656d45df67
kumailalq <kumal-081201@student.jenseneducation.se>
2026-05-08 15:03:57 +0200

    upp 3,4,5,6

--- simple_calculator.js
+++ simple_calculator.js
@@ -4,3 +4,26 @@ function calculateSum() {
     let sum = num1 + num2;
     document.getElementById('result').innerHTML = 'Result: ' + sum;
 }
+
+
+function calculateMin() {
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    let dif = num1 - num2;
+    document.getElementById('result').innerHTML = 'Result: ' + dif;
+}
+
+function calculateMul() {
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    let mul = num1 * num2;
+    document.getElementById('result').innerHTML = 'Result: ' + mul;
+}
+
+
+function calculateDiv() {
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    let div = num1 / num2;
+    document.getElementById('result').innerHTML = 'Result: ' + div;
+}
